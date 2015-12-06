321825524edfa58f448630efe848ea65e6fda906
uri michaeli <uri@slidernet.co>
2015-12-06 10:52:14 +0200

    .wait and .then

--- categoriesSpec.js
+++ categoriesSpec.js
@@ -4,10 +4,17 @@ var chakram = require('chakram'),
 describe("Documentation examples", function() {
     this.timeout(10000);
 
-    it("should support chakram and chai assertions", function () {
+    it("chakram.wait", function () {
         var response = chakram.get("http://ecom.slidernet.co/api/categories/all");
         expect(response).to.have.status(200);
         expect(response).not.to.have.status(404);
         return chakram.wait();
+    });
+    it(".then", function () {
+        return chakram.get("http://ecom.slidernet.co/api/categories/all").then(
+            function(response){
+                expect(response).to.have.status(200);
+                expect(response).not.to.have.status(404);
+            });
     });
 });
