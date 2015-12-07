92a11191bbe48a14a29f6900b2de7ae6cb7fcaa8
uri michaeli <uri@slidernet.co>
2015-12-07 11:30:37 +0200

    fullpath 2

--- build.js
+++ build.js
@@ -1,6 +1,9 @@
 var fs = require('fs');
+var path = require('path');
 var filename = (new Date().getTime().toString());
-fs.writeFile("dist/" + filename + '.txt', "Build succeded " + filename, function(err) {
+var full_path = path.join(__dirname, "dist/" + filename + '.txt');
+
+fs.writeFile(full_path, "Build succeded " + filename, function(err) {
     if(err) {
         return console.log(err);
     }
